017d5a51bbe5e9d1db52be75d3b56ca7a8524642
Suvam-Karmakar <karmakarsuvam16@gmail.com>
2025-06-15 06:01:24 +0530

    End of section 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,17 +1,14 @@
-import { useEffect, useState } from "react"
-import Catalog from "../../features/catalog/Catalog";
-import type { Product } from "../modules/product";
+import { useState } from "react"
 import { Box, Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import NavBar from "./NavBar";
-
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [darkMode, setDarkmode] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
   const palleteType = darkMode ? 'dark' : 'light'
   const theme = createTheme({
     palette: {
-      mode: palleteType, 
+      mode: palleteType,
       background: {
         default: (palleteType === 'light') ? '#eaeaea' : '#121212'
       }
@@ -19,33 +16,29 @@ function App() {
   });
 
   const toggleDarkMode = () => {
-    setDarkmode(!darkMode);
+    setDarkMode(!darkMode);
   }
-
-  useEffect(() => {
-    fetch('https://localhost:5001/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data))
-  }, [])
 
   return (
     <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <NavBar toggleDarkMode={toggleDarkMode} darkmode={darkMode} />
-    <Box 
-      sx={{
-        minHeight: '100vh',
-        background: darkMode 
-          ? 'radial-gardient(circle, #1e3aBa, #111B27)' 
-          : 'radial-gardient(circle, #bacef9, #f0f9ff)',
-        py: 6
-      }}
-    >
-      <Container maxWidth='xl' sx={{mt: 8}}>
-        <Catalog products={products} />
-      </Container>
-    </Box>
+      <CssBaseline />
+      <NavBar toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
+      <Box
+        sx={{
+          minHeight: '100vh',
+          background: darkMode 
+            ? 'radial-gradient(circle, #1e3aBa, #111B27)'
+            : 'radial-gradient(circle, #baecf9, #f0f9ff)',
+          py: 6
+        }}
+      >
+        <Container maxWidth='xl' sx={{ mt: 8 }}>
+          <Outlet />
+        </Container>
+      </Box>
+
     </ThemeProvider>
+
   )
 }
 
