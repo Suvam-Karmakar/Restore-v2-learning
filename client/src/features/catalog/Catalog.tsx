e5653acb2496db84fcab55581f18100e17be320d
Suvam-Karmakar <karmakarsuvam16@gmail.com>
2025-06-23 07:07:03 +0530

    End of section 5

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,20 +1,16 @@
-import { useEffect, useState } from "react";
-import type { Product } from "../../app/modules/product"
 import ProductList from "./ProductList";
+import { useFetchProductsQuery } from "./catalogApi";
 
 
 export default function Catalog() {
-  const [products, setProducts] = useState<Product[]>([]);
 
-  useEffect(() => {
-    fetch('https://localhost:5001/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data))
-  }, [])
+  const {data, isLoading} = useFetchProductsQuery();
+
+  if (isLoading || !data) return <div>Loading...</div>
 
   return (
     <>
-      <ProductList products={products} />
+      <ProductList products={data} />
     </>
   )
 }
